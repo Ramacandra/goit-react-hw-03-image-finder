4e7aaa87e8be6a9fcaf7008ae4bee3f6c7e97a46
Ramacandra <marcelaramacandra@gmail.com>
2024-04-15 22:30:44 -0500

    primer intento

---
+++ src/components/ImageGallery.jsx
@@ -0,0 +1,19 @@
+
+import React from 'react';
+import ImageGalleryItem from './ImageGalleryItem';
+
+import { ImageGalleryContainer } from './ImageGallery.styled';
+
+function ImageGallery({ images, openModal }) {
+    return (
+        <ImageGalleryContainer className="image-gallery">
+            {images.map((image) => (
+                <div className="image-wrapper" key={image.id} onClick={() => openModal(image.largeImageURL, image.altText)}>
+                    <img src={image.webformatURL} alt={image.altText} />
+                </div>
+            ))}
+        </ImageGalleryContainer>
+    );
+}
+
+export default ImageGallery;
